32203f34417d457390ee1343dc61cafa5a69a5ec
fd_bo <fd_bo@robot>
2021-05-05 22:34:30 -0300

    Criando nosso INSERT ao banco de dados Criando nossa consulta SQL ao banco de dados

--- pos-java-jdbc/src/main/java/dao/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -1,8 +1,15 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import conexaojdbc.SingleConnection;
+import model.Userposjava;
+
 /*persitencia*/
 public class UserPosDAO {
 
@@ -12,6 +19,55 @@ public class UserPosDAO {
 
 		connection = SingleConnection.getConnection();
 
+	}
+
+	public void salvar(Userposjava userposjava) {
+		try {
+
+			String sql = "insert into userposjava (id, nome, email) values (?, ?, ?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setLong(1, userposjava.getId());
+			insert.setString(2, userposjava.getNome());
+			insert.setString(3, userposjava.getEmail());
+			insert.execute();
+			connection.commit();// salva no banco
+
+		} catch (Exception e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public List<Userposjava> listar() throws SQLException {
+		
+		List<Userposjava> list = new ArrayList<Userposjava>();
+		
+		String sql = "select * from userposjava";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultado = statement.executeQuery();
+		
+		while (resultado.next()) {
+		
+			Userposjava userposjava = new Userposjava();
+			
+			userposjava.setId(resultado.getLong("id"));
+			userposjava.setNome(resultado.getString("nome"));
+			userposjava.setEmail(resultado.getString("email"));
+			
+			list.add(userposjava);
+		}
+		
+		return list;
+		
 	}
 
 }
--- pos-java-jdbc/src/test/java/posjavajdbc/posjavajdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/posjavajdbc/posjavajdbc/TesteBancoJdbc.java
@@ -1,14 +1,48 @@
 package posjavajdbc.posjavajdbc;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
-
 	@Test
 	public void initBanco() {
 
-		SingleConnection.getConnection();
+		// SingleConnection.getConnection();
+
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+
+		userposjava.setId(5L);
+		userposjava.setNome("Fernando");
+		userposjava.setEmail("ribeiro@rocha.com.br");
+
+		userPosDAO.salvar(userposjava);
+
 	}
+
+	@Test
+	public void initListar() {
+		UserPosDAO dao = new UserPosDAO();
+
+		try {
+
+			List<Userposjava> list = dao.listar();
+
+			for (Userposjava userposjava : list) {
+
+				System.out.println(userposjava);
+				System.out.println("--------------------------------------------------------");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+	}
+
+}
 }
